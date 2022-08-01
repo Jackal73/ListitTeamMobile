dfa0a855cb7e52efb17ca1d4561640fdad70bc82
Shawn Kebel <fxrender@outlook.com>
2022-08-01 16:09:23 -0500

    :construction: Work on main ToDo screen

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,35 @@
-import { StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, FlatList } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import ToDoItem from "../components/ToDoItem";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<"TabOne">) {
+  const [todos, setTodos] = useState([
+    {
+      id: "1",
+      content: "Buy bicycle",
+      isCompleted: true,
+    },
+    {
+      id: "2",
+      content: "Ride new bike",
+      isCompleted: false,
+    },
+    {
+      id: "3",
+      content: "Go to ER",
+      isCompleted: true,
+    },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>Listit Teams</Text>
+
+      <FlatList data={todos} renderItem={({ item }) => <ToDoItem todo={item} />} style={{ width: "100%" }} />
     </View>
   );
 }
@@ -17,16 +37,11 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    padding: 12,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    fontWeight: "bold",
   },
 });
